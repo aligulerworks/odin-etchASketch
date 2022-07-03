2238415d1ecc2ebd8805a97f45cf3498fef921fc
alicaptain <aligulerworks@gmail.com>
2022-07-03 17:54:45 +0300

    A Commit To Consult A  SuperVisor

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
 'use strict';
 
 
+
+
 //Canvas size/resolution
-const cellCount = '1fr 1fr 1fr 1fr 1fr';
+// const cellCount = '1fr 1fr 1fr 1fr 1fr';
 
 
 // Variables for existing elements
 const canvas = document.getElementById('canvas');
 
 //Create Cells to be Painted
-const cell = document.createElement('div');
-cell.classList.add('cell');
-cell.textContent = 'Naber durruk';
+let cell = document.createElement('div');
+let cellCount;
+
+// Cell Filling Function
+function canvasGrid(userCellInput) {
+   let count = userCellInput;
+   cellCount = userCellInput;
+   (userCellInput > count) ? count = userCellInput : count;
+
+   for (let i = 0; i < count; i++) {
+   // Put Cell into canvas
+   cell = canvas.appendChild(document.createElement('div'));
+   cell.classList.add('cell');
+   cell.textContent = 'durruk';
+   Object.assign(cell.style, cellStyles);
+   Object.assign(canvas.style, canvasStyles);
+
+
+   }
+}
+
 
 
 // STYLES
 //style list of cell
 const cellStyles = {
+   'display': 'grid',
+   'place-items': 'center',
    'background-color': 'lightblue',
-   'width': '100px',
-   'height': '100px',
-   'border-radius': '5%',
-   'box-sizing': 'border-box'
+   'width': '60px',
+   'height': '60px',
+   'border-radius': '0.5rem',
+   'box-sizing': 'border-box',
+   'overflow': 'hidden'
+   
 };
 
 // style list of canvas
 const canvasStyles = {
-   'grid-template-columns': 'auto',
-   'grid-template-rows': 'auto',
-   'gap': '2px',
+   'display': 'grid',
+   'width': '100%',
+   'height': '100%',
+   'gap': '4px',
+   'grid-template-columns': '10fr',
 }
 
 //ASSIGN LISTED STYLES
 // assign styles to canvas
 Object.assign(canvas.style, canvasStyles);
 //assign styles to cell
-Object.assign(cell.style, cellStyles);
+// Object.assign(cell.style, cellStyles);
 
 
-// Put Cell into canvas
-canvas.appendChild(cell);
 
 
 
@@ -47,3 +71,5 @@ canvas.appendChild(cell);
 
 
 
+//TESTS
+canvasGrid(12);
